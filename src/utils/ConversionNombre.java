ccd488ff363b4b671730fdda37cf5092b481e042
alanquentin85 <alan?quentin@epsi.fr>
2020-10-28 14:40:55 +0100

    fin du tp4

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ConversionNombre {
+    public static void main(String[] args) {
+        String variable = "34";
+        int i = Integer.parseInt(variable);
+        System.out.println(i);
+        int a = 56;
+        int b = 45;
+        int resultat = Integer.max(a,b);
+        System.out.println(resultat);
+    }
+}
