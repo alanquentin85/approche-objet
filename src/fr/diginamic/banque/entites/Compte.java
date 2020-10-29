da0df2082cade72528746ce818716a761137cf6b
alanquentin85 <alan?quentin@epsi.fr>
2020-10-29 08:14:23 +0100

    fin du tp6

--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -8,4 +8,9 @@ public class Compte {
         this.num = num;
         this.solde = solde;
     }
+    public String toString() {
+        return "Compte :" +
+                "num='" + num + '\'' +
+                ", solde=" + solde;
+    }
 }
--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -3,8 +3,7 @@ package fr.diginamic.banque.entites;
 public class TestBanque {
     public static void main(String[] args) {
         Compte firstCompte = new Compte("123 007 333 443 558 676",4900.56F);
-        System.out.println(firstCompte.num);
-        System.out.println(firstCompte.solde);
+        System.out.println(firstCompte.toString());
 
     }
 }
